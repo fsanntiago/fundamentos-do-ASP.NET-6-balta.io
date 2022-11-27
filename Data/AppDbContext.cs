195eef1a412ca6804b56da80cd9737aeb1e3807e
fsanntiago <zerofabricio@hotmail.com>
2022-11-27 18:03:37 -0300

    set up EF

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Todo.Models;
+
+namespace Todo.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<TodoModel> TodoModels { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("DataSource=app.db;Cache=Shered");
+    }
+}
