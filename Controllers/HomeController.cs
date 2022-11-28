a50aa7755cda3c7418f7aaff7449ad1047f215f7
fsanntiago <zerofabricio@hotmail.com>
2022-11-28 09:45:55 -0300

    routes and controllers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 namespace Todo.Controllers;
 
 [ApiController]
+[Route("home")]
 public class HomeController : ControllerBase
 {
     [HttpGet]
+    [Route("/")]
     public string Get()
     {
         return "Hello World";
