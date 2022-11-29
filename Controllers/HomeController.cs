73d85b83e58128f12ef9784c6f8b2f4754e37b8e
fsanntiago <zerofabricio@hotmail.com>
2022-11-29 10:22:24 -0300

    add put and delete

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,19 +7,48 @@ namespace Todo.Controllers;
 [ApiController]
 public class HomeController : ControllerBase
 {
-    [HttpGet]
-    [Route("/")]
-    public List<TodoModel> Get([FromServices]AppDbContext context)
+    [HttpGet("/")]
+    public List<TodoModel> Get([FromServices] AppDbContext context)
     {
         return context.TodoModels.ToList();
     }
-    
-    [HttpPost]
-    [Route("/")]
-    public TodoModel Post([FromBody]TodoModel todo, [FromServices]AppDbContext context)
+
+    [HttpGet("/{id:int}")]
+    public TodoModel GetById([FromRoute] int id, [FromServices] AppDbContext context)
+    {
+        return context.TodoModels.FirstOrDefault(x => x.Id == id);
+    }
+
+    [HttpPost("/")]
+    public TodoModel Post([FromBody] TodoModel todo, [FromServices] AppDbContext context)
     {
         context.TodoModels.Add(todo);
         context.SaveChanges();
         return todo;
+    }
+    
+    [HttpPut("/{id:int}")]
+    public TodoModel Put([FromRoute]int id, [FromBody] TodoModel todo, [FromServices] AppDbContext context)
+    {
+        var model = context.TodoModels.FirstOrDefault(x => x.Id ==  id);
+        if (model == null)
+            return todo;
+        
+        // Comparar os valores que vem da tabela com que veio do banco 
+        model.Title = todo.Title;
+        model.Done = todo.Done;
+
+        context.TodoModels.Update(model);
+        context.SaveChanges();
+        return model;
+    }
+    
+    [HttpDelete("/{id:int}")]
+    public TodoModel Delete([FromRoute]int id, [FromServices] AppDbContext context)
+    {
+        var model = context.TodoModels.FirstOrDefault(x => x.Id ==  id);
+        context.TodoModels.Remove(model);
+        context.SaveChanges();
+        return model;
     }
 }
